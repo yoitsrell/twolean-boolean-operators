b286e2c5e1b0d9aeac390f7efbd736629c475aff
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-04 11:06:31 -0500

    intial

--- main.js
+++ main.js
@@ -2,10 +2,61 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(one){
+  return one > 5
+}
 
+function topScore(two, three){
+  return two > three
+}
 
+function isInDanger(grade1){
+  return grade1 >= 60 && grade1 <= 71;
+}
 
+function isCoasting (grade2){
+  return grade2 >= 72 && grade2 <= 83
+}
 
+function isSucceeding (grade3){
+  return grade3 >= 84 && grade3 <= 92
+}
+
+function isFailing (grade4){
+  return grade4 <=59
+}
+
+function isAcing (grade5){
+  return grade5 >92
+}
+
+function isStudent (student){
+  return student === 'student'
+}
+
+function isTeacher (teach){
+  return teach === 'teacher'
+}
+
+function isAdmin (admin){
+  return admin === 'admin'
+}
+
+function isElementary(elem){
+  return elem === 'elementary'
+}
+
+function isMiddleSchoolTeacher(mid, mid2){
+  return mid === 'teacher' && mid2 >= 6 && mid2 <= 8 
+}
+
+function notAnElementarySchoolAdministrator(admin1,admin2){
+  return admin1 !== 'elementary' || admin2 == 'teacher'
+}
+
+function differentPeople(name1,name2){
+  return name1 !== name2
+}
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
  *                              THIS IS FOR INTERNAL USE ONLY                               *
